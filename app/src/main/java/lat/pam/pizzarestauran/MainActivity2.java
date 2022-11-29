9d8dcae3f571fd6a795fb158854ad5b64becbd90
aqsal <muhammadaqsal96@gmail.com>
2022-11-29 15:17:05 +0700

    first commit

---
+++ app/src/main/java/lat/pam/pizzarestauran/MainActivity2.java
@@ -0,0 +1,17 @@
+package lat.pam.pizzarestauran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    private TextView tvl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        tvl = (TextView)findViewById(R.id.hello_asep);
+    }
+}
